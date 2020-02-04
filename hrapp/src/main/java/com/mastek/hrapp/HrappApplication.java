51fa994b610fef2e59dd3162de1fed92609b93f3
amy61289 <amy61289@GB-LD1LPW10G006.mastek.com>
2020-02-04 14:55:11 +0000

    JEE Spring Boot Project Started

---
+++ hrapp/src/main/java/com/mastek/hrapp/HrappApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.hrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrappApplication.class, args);
+	}
+
+}
