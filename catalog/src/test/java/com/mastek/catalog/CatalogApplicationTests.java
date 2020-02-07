57a8a9d78ba76e43e82fb3c2ae9c9618504990cc
Amy61289 <Amy61289@GB-LD1LPW10G006.mastek.com>
2020-02-07 15:50:36 +0000

    Uploading catalogue

---
+++ catalog/src/test/java/com/mastek/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastek.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
