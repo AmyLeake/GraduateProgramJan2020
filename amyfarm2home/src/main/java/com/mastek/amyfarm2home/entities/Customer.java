1b418999eac705a75e3160e9a00abc31faf2af38
Amy61289 <Amy61289@GB-LD1LPW10G006.mshome.net>
2020-09-08 14:04:11 +0100

    Completed customer and order entities. Created enum for subscription type and linked to customer.

--- amyfarm2home/src/main/java/com/mastek/amyfarm2home/entities/Customer.java
+++ amyfarm2home/src/main/java/com/mastek/amyfarm2home/entities/Customer.java
@@ -1,5 +1,109 @@
 package com.mastek.amyfarm2home.entities;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Customer {
+	
+	int customerID;
+	String customerName;
+	String address;
+	String email;
+	int contactNumber;
+	SubscriptionType subscriptionType;
+	
+	public Customer() {
+
+	}
+
+	@Id
+	@Column(name="customer_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	@Column(name="customer_name")
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name="contact_number")
+	public int getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(int contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	@Column(name="subscription_type")
+	@Enumerated(EnumType.STRING)
+	public SubscriptionType getSubscriptionType() {
+		return subscriptionType;
+	}
+
+	public void setSubscriptionType(SubscriptionType subscriptionType) {
+		this.subscriptionType = subscriptionType;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + customerID;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (customerID != other.customerID)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerID=" + customerID + ", customerName=" + customerName + ", address=" + address
+				+ ", email=" + email + ", contactNumber=" + contactNumber + ", subscriptionType=" + subscriptionType
+				+ "]";
+	}
+	
+	
+	
 }
---
+++ amyfarm2home/src/main/java/com/mastek/amyfarm2home/entities/SubscriptionType.java
@@ -0,0 +1,5 @@
+package com.mastek.amyfarm2home.entities;
+
+public enum SubscriptionType { MONTHLY, WEEKLY, ONE_OFF
+
+}
